30142cfe8f9e5b346343a70beaf5d536b0a95cea
HenryCastel <162947222+HenryCastel@users.noreply.github.com>
2026-04-18 23:27:36 -0500

    Add files via upload

---
+++ getcolor.ps1
@@ -0,0 +1,5 @@
+Add-Type -AssemblyName System.Drawing
+$img = [System.Drawing.Image]::FromFile('C:\xampp\htdocs\Pg_ProAlar_Anti\Logo_Pro.png')
+$bmp = New-Object System.Drawing.Bitmap($img)
+$c = $bmp.GetPixel(0,0)
+Write-Host "#$($c.R.ToString('X2'))$($c.G.ToString('X2'))$($c.B.ToString('X2'))"
